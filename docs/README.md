5494431e435bc076955cba46417f2af94b83b4f2
Rory Flint <rory.flint@tipalti.com>
2024-10-07 15:51:04 +0100

    add ts-up

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,3 @@
-# Headline
+# cipherTS
 
 > An awesome project.
---
+++ src/ciphers/index.ts
@@ -0,0 +1,7 @@
+export * from './affine';
+export * from './atbash';
+export * from './autokey';
+export * from './caesar';
+export * from './rot13';
+export * from './substitution';
+export * from './vigenere';
